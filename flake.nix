b7313b6be47e0b8e3af2a4f14382d6d5f51c3f53
Daylin Morgan <daylinmorgan@gmail.com>
2024-02-22 13:06:40 -0600

    test

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   }: let
     inherit (nixpkgs.lib) genAttrs makeBinPath;
     forAllSystems = f:
-      genAttrs [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"]
+      genAttrs ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"]
       (system:
         f (import nixpkgs {
           localSystem = system;
@@ -15,13 +15,12 @@
   in {
     devShells = forAllSystems (pkgs: {
       default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              pandoc
-              (texlive.combine { inherit (texlive) scheme-small adjustbox datetime2; })
-              (python3.withPackages (packages: with packages; [ pip ]))
-            ];
+        nativeBuildInputs = with pkgs; [
+          pandoc
+          (texlive.combine {inherit (texlive) scheme-small adjustbox datetime2;})
+          (python3.withPackages (packages: with packages; [pip]))
+        ];
       };
     });
   };
 }
-

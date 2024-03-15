e059524b4124df90e653516e7d4435d4def3269f
Daylin Morgan <daylinmorgan@gmail.com>
2024-03-15 15:07:38 -0500

    add note about packages

--- flake.nix
+++ flake.nix
@@ -9,23 +9,52 @@
     forAllSystems = f:
       genAttrs ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"]
       (system:
-        f (import nixpkgs {
-          localSystem = system;
-        }));
+        f (import nixpkgs {inherit system;}));
   in {
     devShells = forAllSystems (pkgs: {
       default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
-          pandoc
-          (texlive.combine {inherit (texlive) 
-          scheme-small lualatex-math selnolig siunitx 
-
-          adjustbox babel-german background bidi collectbox csquotes everypage filehook
-          footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights
-          needspace pagecolor sourcecodepro sourcesanspro titling ucharcat ulem
-          unicode-math upquote xecjk xurl zref
-          ;})
           (python3.withPackages (packages: with packages; [pip]))
+          pandoc
+          (texlive.combine {
+            inherit
+              (texlive)
+              scheme-small
+              lualatex-math
+              selnolig
+              siunitx
+              # eisvogel template packages
+              
+              adjustbox
+              babel-german
+              background
+              bidi
+              collectbox
+              csquotes
+              everypage
+              filehook
+              footmisc
+              footnotebackref
+              framed
+              fvextra
+              letltxmacro
+              ly1
+              mdframed
+              mweights
+              needspace
+              pagecolor
+              sourcecodepro
+              sourcesanspro
+              titling
+              ucharcat
+              ulem
+              unicode-math
+              upquote
+              xecjk
+              xurl
+              zref
+              ;
+          })
         ];
       };
     });
